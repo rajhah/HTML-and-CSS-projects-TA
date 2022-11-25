b5b42e317b6061ae3790b71431d33c553f6f9be9
rajhah <109645238+rajhah@users.noreply.github.com>
2022-11-25 07:22:38 -0600

    Added basic-html folder to project

---
+++ Basic-HTML-and-JavaScript-1/Basic-JavaScript-1.js
@@ -0,0 +1,7 @@
+alert("JS alert")
+
+function My_First_Function() {
+	var stringText = "Kiss me, I'm Irish";
+	var result = stringText.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
